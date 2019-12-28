394bbe4acb2e10ff7e19c78ff9c411bd2c6cfcbc
Tyler Thomasson <thomassontyler@gmail.com>
2019-12-28 16:39:27 -0500

    things

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,30 @@
-console.log('Hello');
-const myHeading = document.querySelector('h2');
-myHeading.textContent = 'Hello world!';
+let myImage = document.querySelector('img');
+
+myImage.onclick = function(){
+	let mySrc = myImage.getAttribute('src');
+	if(mySrc === 'images/reel.png'){
+		myImage.setAttribute ('src', 'images/reel2.png');
+	} else {
+		myImage.setAttribute ('src', 'images/reel.png');
+	}
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+	let myName = prompt('Please enter your name.');
+	localStorage.setItem('name', myName);
+	myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+	setUserName();
+} else {
+	let storedName = localStorage.getItem('name');
+	myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+	setUserName();
+}
